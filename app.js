d0db033760a931f2836243dba6f6f389a7c11318
Zee Celest <zeecelest@gmail.com>
2019-09-16 19:14:29 -0400

    leapyear drill

--- app.js
+++ app.js
@@ -32,24 +32,25 @@ console.log(beyond(-5));
 // check first letter
 // a,b,c,d = 2,3,4,5
 // get letter at position, if empty = space
-function decode(string){
+function decode(string) {
   // split input string into words
   let words = string.split(' ');
   //  create empty string to store return characters
   let returnString = '';
 
   //  loop through words
-  for (let i=0; i < words.length; i++) {
+  for (let i = 0; i < words.length; i++) {
     // check if current 0 character == 'a'/'b'/'c'/'d'
     if (words[i][0] === 'a') {
-      returnString += (words[i][1]);
+      returnString += words[i][1];
     } else if (words[i][0] === 'b') {
-      returnString += (words[i][2]);
+      returnString += words[i][2];
     } else if (words[i][0] === 'c') {
-      returnString += (words[i][3]);
+      returnString += words[i][3];
     } else if (words[i][0] === 'd') {
-      returnString += (words[i][4]);
-    } else { // else return a space
+      returnString += words[i][4];
+    } else {
+      // else return a space
       returnString += ' ';
     }
   }
@@ -58,19 +59,22 @@ function decode(string){
 
 decode('craft block argon meter bells brown croon droop');
 
-
 //  How Many days in a month
+// function w/ 2 arguments month ('') and leapYear(boolean)
+// use switch statement to return sentence showing how many days in that month
+
+function daysInMonth(month, leapYear) {}
 
 //  Rock Paper Scissors
 // r=1
 // p=2
 // s=3
-function RPS(playerInput){
+function RPS(playerInput) {
   const randomNo = Math.floor(Math.random() * 3) + 1;
 
   if (randomNo === playerInput) {
     console.log('tie');
-  } else if ( randomNo === 1 && playerInput === 2) {
+  } else if (randomNo === 1 && playerInput === 2) {
     console.log('player wins');
   } else if (randomNo === 1 && playerInput === 3) {
     console.log('computer wins');
