9248dc4086546586cce2bc74df228433741fc343
Bridger <bridgerrhammond@gmail.com>
2019-09-16 16:21:50 -0500

    comments

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 'use strict';
 
+// Jedi Name
 function jediName(firstName, lastName) {
   return(lastName.substring(0,3) + firstName.substring(0,2));
 }
 
 let getName = jediName('Bridger', 'Hammond');
 console.log(getName);
+
+//  To infinity...
+
+//  Cracking the code
+
+//  How Many days in a month
+
+//  Rock Paper Scissors
