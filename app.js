810df6ee87296424a22cb669b2da0060e827e8f4
Zee Celest <zeecelest@gmail.com>
2019-09-16 17:50:27 -0400

    infinity drill added

--- app.js
+++ app.js
@@ -2,13 +2,29 @@
 
 // Jedi Name
 function jediName(firstName, lastName) {
-  return(lastName.substring(0,3) + firstName.substring(0,2));
+  return lastName.substring(0, 3) + firstName.substring(0, 2);
 }
 
 let getName = jediName('Bridger', 'Hammond');
 console.log(getName);
 
 //  To infinity...
+function beyond(num) {
+  if (num === 0) {
+    return 'Staying home.';
+  }
+  if (num === Infinity) {
+    return 'And beyond.';
+  } else {
+    if (num > 0) {
+      return 'To infinity';
+    }
+    if (num < 0) {
+      return 'To negative infinity.';
+    }
+  }
+}
+console.log(beyond(-5));
 
 //  Cracking the code
 
